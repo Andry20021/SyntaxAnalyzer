9a20b5197d49b8398c8b5bcb716179642c6c02a6
Charles Eubanks <146963974+Eubanksch81@users.noreply.github.com>
2025-03-27 15:39:00 -0500

    Setting up methods
    Because formatting is important! :)

--- SyntaxAnalyzer.cpp
+++ SyntaxAnalyzer.cpp
@@ -1,22 +1,48 @@
-#include <iostream>
-#include <vector>
-#include <map>
-using namespace std;
 #include "SyntaxAnalyzer.h"
 
 
-    int vdecassign ();    // andry
-    bool stmtlist();     // charles
-    int stmt();           // ash
-    bool ifstmt();        // andry
-    bool elsepart();      // ash
-    bool whilestmt();           // charles
-    bool assignstmt();          // ash
-    bool inputstmt();           // andry
-    bool outputstmt();          // charles
-    bool expr();                // andry
-    bool simpleexpr();          // ash
-    bool term();                // charles
-    bool logicop();             // andry
-    bool arithop();             // charles
-    bool relop();               // ash
+    int SyntaxAnalyzer::vdecassign () {         // andry
+
+    }
+    bool SyntaxAnalyzer::stmtlist() {           // charles
+
+    }
+    int SyntaxAnalyzer::stmt() {                // ash
+
+    }
+    bool SyntaxAnalyzer::ifstmt(){              // andry
+
+    }
+    bool SyntaxAnalyzer::elsepart(){            // ash
+
+    }
+    bool SyntaxAnalyzer::whilestmt(){           // charles
+
+    }
+    bool SyntaxAnalyzer::assignstmt(){          // ash
+
+    }
+    bool SyntaxAnalyzer::inputstmt(){           // andry
+
+    }
+    bool SyntaxAnalyzer::outputstmt(){          // charles
+
+    }
+    bool SyntaxAnalyzer::expr(){                // andry
+
+    }
+    bool SyntaxAnalyzer::simpleexpr(){          // ash
+
+    }
+    bool SyntaxAnalyzer::term(){                // charles
+
+    }
+    bool SyntaxAnalyzer::logicop(){             // andry
+
+    }
+    bool SyntaxAnalyzer::arithop(){             // charles
+
+    }
+    bool SyntaxAnalyzer::relop(){               // ash
+
+    }
